c8038fc60c78bbf087c1158249cf4b2172a569c4
KevinnFigueroa <kevinfigueroadavid2000@gmail.com>
2021-03-26 04:09:51 -0300

    feat: añadiendo api de oxford como route

--- backend/index.py
+++ backend/index.py
@@ -13,14 +13,27 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from db.database import initDb
 
+import os
+from firebase_admin import credentials, firestore, initialize_app
+
+from services.get_words_api import RandomTrivia
+
 app = Flask(__name__)
 
 initDb(app)
 
+# Initialize Firestore DB
+cred = credentials.Certificate("./serviceAccountKey.json")
+default_app = initialize_app(cred)
+db = firestore.client()
+users = db.collection("users")
+
+# Initialize Trivia API
+instance = RandomTrivia()
+
 
 @app.route("/", methods=["GET"])
 def index():
-    print("ahre")
     return render_template("index.html")
 
 
@@ -33,11 +46,26 @@ def login():
     return response
 
 
+@app.route("/list", methods=["GET"])
+def read():
+    """
+    read() : Fetches documents from Firestore collection as JSON
+    todo : Return document that matches query ID
+    all_todos : Return all documents"""
+    try:
+        all_todos = [doc.to_dict() for doc in users.stream()]
+        return jsonify(all_todos), 200
+
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+
 # TODO: para establecer los 5 mejores debo usar signals
 
 
 @app.route("/ranking", methods=["GET"])
 def ranking():
+    # Hacer consulta a firebase de los users que tienen qualification más altos
     data = {"topFive": ["kevin", "renzo", "german", "pablo", "walter"]}
     response = app.response_class(
         response=json.dumps(data), status=200, mimetype="application/json"
@@ -47,12 +75,20 @@ def ranking():
 
 @app.route("/random_trivia", methods=["GET"])
 def random_trivia():
-    data = {"Amor": "Palabra de afecto hacia otra persona"}
+    trivias = instance.get_random_trivia()
 
-    response = app.response_class(
-        response=json.dumps(data), status=200, mimetype="application/json"
-    )
-    return response
+    if trivias:
+        response = app.response_class(
+            response=json.dumps(trivias), status=200, mimetype="application/json"
+        )
+        return response
+    else:
+        response = app.response_class(
+            response=json.dumps({"Success": False}),
+            status=404,
+            mimetype="application/json",
+        )
+        return response
 
 
 if __name__ == "__main__":
--- backend/services/get_words_api.py
+++ backend/services/get_words_api.py
@@ -1,36 +1,64 @@
 import requests
 import json
+import os
+import random
+from random import randint
 
-# credenciales de la api
-app_id = "cabc512f"
-app_key = "3b63add2d492321f11b2cc2d2acabe58"
 
-# configuraciones de la request
+class RandomTrivia:
+    def __init__(self):
+        """
+        credenciales de la api
+        """
+        self.app_id = "cabc512f"
+        self.app_key = "3b63add2d492321f11b2cc2d2acabe58"
+        self.language = "es"
+        self.fields = "definitions"
+        self.strictMatch = "false"
 
-language = "es"
-word_id = "amor"
-fields = "definitions"
-strictMatch = "false"
+    def get_random_trivia(self):
+        """
+        TODO: Mejorar esta implementacion guardando en bd
+        """
+        lines = open("services/words.txt", encoding="utf8").read().splitlines()
+        words_parsed = []
+        response_data = []
 
-url = (
-    "https://od-api.oxforddictionaries.com:443/api/v2/entries/"
-    + language
-    + "/"
-    + word_id.lower()
-    + "?fields="
-    + fields
-    + "&strictMatch="
-    + strictMatch
-)
+        for line in lines:
+            if line == "":
+                continue
+            else:
+                words_parsed.append(line)
 
-r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
+        for num in range(0, 5):
+            random_word = words_parsed[randint(0, len(words_parsed) - 1)]
+            url = (
+                "https://od-api.oxforddictionaries.com:443/api/v2/entries/"
+                + self.language
+                + "/"
+                + random_word.lower()
+                + "?fields="
+                + self.fields
+                + "&strictMatch="
+                + self.strictMatch
+            )
 
-print("code {}\n".format(r.status_code))
-print("text \n" + r.text)
+            r = requests.get(
+                url, headers={"app_id": self.app_id, "app_key": self.app_key}
+            )
 
-response = r.json()
-response = response["results"][0]["lexicalEntries"][0]["entries"][0]["senses"]
+            response = r.json()
+            if "results" in response:
+                definitions = []
+                response = response["results"][0]["lexicalEntries"][0]["entries"][0][
+                    "senses"
+                ]
 
-# funcion que trae todas las definiciones de la palabra
-for definicion in range(0, len(response)):
-    print(response[definicion]["definitions"][0] + "\n")
+                # funcion que trae todas las definiciones de la palabra
+                for definicion in range(0, len(response)):
+                    definitions.append(response[definicion]["definitions"][0])
+
+                random_word_data = {"word": random_word, "definitions": definitions}
+                response_data.append(random_word_data)
+
+        return response_data
